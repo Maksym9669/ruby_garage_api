ab19fd98c57f79c6487d803cc388f317efa1296f
Maksym Sychov <maksym.sychov41@gmail.com>
2020-06-19 21:32:18 +0300

    Create service objects for moving tasks

--- app/controllers/api/priority_down_controller.rb
+++ app/controllers/api/priority_down_controller.rb
@@ -3,29 +3,18 @@ module Api
         before_action :authenticate_user!
         
         def update
-            @task = Task.find_by(id: params[:id])
-            authorize @task
-            @tasks = Task.where("project_id = ?", params[:project_id]).order(priority: :desc)
-            taskIndex = @tasks.index(@task)
-            if @task
-                if taskIndex != @tasks.length - 1
-                    move_down(taskIndex)
-                    render json: { success: { text: "Task was moved down" } }, status: 200
-                else
-                    render json: { "error": "This task can't be moved down" }, status: 400
-                end
-            else
-              render json: { "error": "Task not found" }, status: 404
+            task_mover = TaskMover.new(params[:id], params[:project_id])
+            task =  Task.find_by(id: params[:id])
+            authorize task
+            result = task_mover.move_down
+            case result
+            when 1
+                render json: { success: { text: "Task was moved down" } }, status: 200
+            when 2
+                render json: { "error": "This task can't be moved down" }, status: 400
+            when 3
+                render json: { "error": "Task not found" }, status: 404
             end
          end
-      
-        private
-
-        def move_down(index)
-            task = @tasks[index+1]
-            temp = @task[:priority]
-            @task.update(priority: task[:priority])
-            task.update(priority: temp)
-        end
     end
 end
--- app/controllers/api/priority_up_controller.rb
+++ app/controllers/api/priority_up_controller.rb
@@ -3,33 +3,18 @@ module Api
         before_action :authenticate_user!
 
          def update
-            @task = Task.find_by(id: params[:id])
-            authorize @task
-            @tasks = Task.where("project_id = ?", params[:project_id]).order(priority: :desc)
-            taskIndex = @tasks.index(@task)
-            if @task
-                if taskIndex != 0
-                    move_up(taskIndex)
-                    render json: { success: { text: "Task was moved up" } }, status: 200
-                else
-                    render json: { "error": "This task can't be moved up" }, status: 400
-                end
-            else
-              render json: { "error": "Task not found" }, status: 404
+            task_mover = TaskMover.new(params[:id], params[:project_id])
+            task =  Task.find_by(id: params[:id])
+            authorize task
+            result = task_mover.move_up
+            case result
+            when 1
+                render json: { success: { text: "Task was moved up" } }, status: 200
+            when 2
+                render json: { "error": "This task can't be moved up" }, status: 400
+            when 3
+                render json: { "error": "Task not found" }, status: 404
             end
          end
-      
-        private
-      
-        def task_params
-         params.permit(:status)
-        end
-
-        def move_up(index)
-            task = @tasks[index-1]
-            temp = @task[:priority]
-            @task.update(priority: task[:priority])
-            task.update(priority: temp)
-        end
     end
 end
