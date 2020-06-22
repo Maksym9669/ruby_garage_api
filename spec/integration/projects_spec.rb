0ea8a97580437ceac48d2a77ba8aea16af9cc379
Maksym Sychov <maksym.sychov41@gmail.com>
2020-06-22 22:30:31 +0300

    Add integration spec for projects

--- spec/integration/projects_spec.rb
+++ spec/integration/projects_spec.rb
@@ -9,7 +9,6 @@ describe 'Projects API' do
         parameter name: :id, in: :path, type: :string
   
         response '200', 'Projects found' do
-          schema type: :array
           run_test!
         end
   
@@ -107,6 +106,23 @@ describe 'Projects API' do
   end
 
 
+  path '/api/projects/{id}' do
 
+    delete 'Delete a project' do
+      tags 'Project'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string
+
+      response '200', 'Project was successfully deleted' do
+        let(:id) { Project.create(name: 'project1').id }
+        run_test!
+      end
+
+      response '404', 'Project not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+  end
   
 end
